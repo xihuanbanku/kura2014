3d2275851757fb5996dffc062325eda769b11a3f
xihuanbanku <lianghaikun@qq.com>
2018-06-07 18:34:09 +0800

    ip分组统计

--- IdcLogStreaming.scala
+++ IdcLogStreaming.scala
@@ -1,48 +1,113 @@
-package com.isinonet.rt
+package com.isinonet.ismartnet
 
-import com.alibaba.fastjson.{JSON, JSONObject}
-import kafka.serializer.StringDecoder
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Durations, StreamingContext}
+import java.util.Properties
+
+import com.isinonet.ismartnet.beans.IdcDaily
+import com.isinonet.ismartnet.mapper.IdcDailyMapper
+import com.isinonet.ismartnet.utils.{JDBCHelper, PropUtils}
+import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.sql.functions._
 
 import scala.collection.mutable.ListBuffer
 
-object IdcLogStreaming {
+/**
+  * idc日志分析
+  * Created by Administrator on 2018-06-06.
+  * 2018-06-06
+  */
+object LogStats {
+
 
   def main(args: Array[String]): Unit = {
-    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val sparkSession = SparkSession.builder()
+      .config("spark.sql.shuffle.partitions", "10")
+      .master("local[*]").appName("LogStats").getOrCreate()
+
+    //导入spark的隐式转换
+    import sparkSession.implicits._
+    //scala  转为java 的集合
+    val date = args(0)
+    //读取日志文件
+    val logToday = sparkSession.read.json("hdfs://192.168.1.200:9000/lhk/" + date + "-11")
 
 
-    val conf: SparkConf = new SparkConf().setMaster("local")
-      .setAppName("IdcLogStreaming")
-    val ssc = new StreamingContext(conf, Durations.seconds(5))
-
-    val brokers = "docker5:9092,docker6:9092,docker7:9092";
-    val topics = "ismartnet.iprobe";
-    val topicSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicSet)
-
-    val lines = messages.map(_._2).map(JSON.parseObject(_)).filter(_.containsKey("host"))
-
-    val pv = lines.map(x => (x.getString("host"), 1)).reduceByKey(_+_)
-    val uv = lines.map(x => (x.getString("sip"), 1)).reduceByKey(_+_).count()
-
-
-    pv.foreachRDD(_.foreachPartition((it) => {
-//        val mapper = session.getMapper(classOf[])
-      val list = ListBuffer[Tuple2[String, String]]()
-      while (it.hasNext) {
-        val unit = it.next()
-//        list.+=(("", ""))
-      }
-//        mapper.insertBatch(list)
+//    logToday.filter($"host" =!= "")
+//      .select($"host").distinct().rdd.foreachPartition((it) => {
+//        val session = JDBCHelper.getSession
+//        val mapper = session.getMapper(classOf[WebsiteMapper])
+//        var i = 0
+//        val list = ListBuffer[Website]()
+//        while (it.hasNext) {
+//          val row = it.next()
+//
+//          val unit = new Website
+//          unit.setDomain(row.getString(0))
+//
+//          list+=(unit)
+//          println(i)
+//          i += 1
+//        }
+//      mapper.insertBatch(list)
 //      session.commit
-    }))
-    ssc.start()
-    ssc.awaitTermination()
-    ssc.clone()
+//    })
+    val props: Properties = PropUtils.loadProps("jdbc.properties")
+    //读取ip RDD
+    val tb_static_ip = sparkSession.read.jdbc(props.getProperty("url"),
+      "tb_static_ip",
+      props).limit(1000).collect()
+    //广播
+    val b_tb_static_ip = sparkSession.sparkContext.broadcast(tb_static_ip)
+    //读取ua RDD
+    val tb_static_uatype = sparkSession.read.jdbc(props.getProperty("url"),
+      "tb_static_uatype",
+      props)
+    //广播
+    val b_tb_static_uatype = sparkSession.sparkContext.broadcast(tb_static_uatype)
+    //读取website RDD
+    val tb_idc_website = sparkSession.read.jdbc(props.getProperty("url"),
+      "tb_idc_website",
+      props).select($"website_id", $"domain")
+    //广播
+    val b_tb_idc_website = sparkSession.sparkContext.broadcast(tb_idc_website)
+
+    //统计pv, uv
+    val pv_uv = logToday.filter($"host" =!= "")
+      .select($"host", $"sip", $"ua")
+//      .groupBy($"host", $"ua")
+//      .agg(count($"sip").as("pv"), countDistinct($"sip").as("uv"), countDistinct($"ua").as("ua"))
+
+//    pv_uv.show(1000, false)
+
+    val pv_uv_ua_website = pv_uv.join(b_tb_static_uatype.value, $"ua" === $"p_type", "left")
+      .join(b_tb_idc_website.value, $"host" === $"domain", "left")
+
+    //确定ip归属地
+    pv_uv_ua_website.mapPartitions((it) => {
+      val ipDB = b_tb_static_ip.value
+      val list = ListBuffer[IdcDaily]()
+
+      val session = JDBCHelper.getSession
+      val mapper = session.getMapper(classOf[IdcDailyMapper])
+      while (it.hasNext) {
+        val e = new IdcDaily
+        val row = it.next()
+        val sip = row.getLong(1)
+
+        //查找对应的IP归属地
+        ipDB.filter(row => sip >= row.getLong(1)&& sip <= row.getLong(2)).take(1).foreach(x=>{
+          e.setProvinceId(x.getInt(0).toShort)
+          e.setIsp(x.getLong(1).toString)
+        })
+        e.setWebsiteId(row.getInt(8))
+        e.setIsMobile(row.getShort(5))
+
+        list.+=(e)
+
+      }
+      list.toIterator
+    })(Encoders.bean(classOf[IdcDaily])).toDF().groupBy($"isMobile", $"isp", $"provinceId", $"websiteId")
+      .agg(countDistinct($"sip").as("uv"), count($"sip").as("pv")).show(false)
+    sparkSession.stop()
   }
 }
